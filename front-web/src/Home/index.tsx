afc2c9464ab3510c8b064bf4efee8291dc946986
WILLX Xavier <willxis@gmail.com>
2021-01-08 13:09:43 -0300

    Add Footer

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,26 +1,30 @@
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg';
+import Footer from '../Footer';
 function Home() {
     return (
-        <div className="home-container">
-            <div className="home-content">
-                <div className="home-actions">
-                    <h1 className="home-title">
-                        Faça o teu pedido <br /> que entregamos <br /> pra você!!!
-                    </h1>
-                    <h3 className="home-subtitle">
-                        Escolha o seu pedido em poucos minutos <br />
-                        levaremos a sua porta
-                    </h3>
-                    <a href="orders" className="home-btn-order">
-                        FAZER PEDIDO
-                    </a>
-                </div>
-                <div className="home-image">
-                    <MainImage />
+        <>
+            <div className="home-container">
+                <div className="home-content">
+                    <div className="home-actions">
+                        <h1 className="home-title">
+                            Faça o teu pedido <br /> que entregamos <br /> pra você!!!
+                        </h1>
+                        <h3 className="home-subtitle">
+                            Escolha o seu pedido em poucos minutos <br />
+                            levaremos a sua porta
+                        </h3>
+                        <a href="orders" className="home-btn-order">
+                            FAZER PEDIDO
+                        </a>
+                    </div>
+                    <div className="home-image">
+                        <MainImage />
+                    </div>
                 </div>
             </div>
-        </div>
+            <Footer />
+        </>
     )
 }
 
